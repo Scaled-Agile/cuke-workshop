12fb4f810115c0808e05374b613a2ba84755d849
Adrian Yong <adrianyong3@gmail.com>
2017-03-25 18:33:54 +1100

    4 - Add step definition file

---
+++ cucumber/features/step_definitions/main_step_defs.rb
@@ -0,0 +1,15 @@
+Given(/^I am on the main game page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I put my name as "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I give the right answers$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should see a message "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
